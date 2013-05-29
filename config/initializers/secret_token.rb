c13d882f54abfd5da432147a5e9a5042a7882b02
Andre Jaenisch <AndreJaenisch@googlemail.com>
2013-05-29 21:24:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MareNostrum::Application.config.secret_token = '27818a535cd5e998d6ace90e2956bede6b8c4e878bea148b479e683ff5f4225168ef691fbb6dc922d9c2b30dd777746b02a38dbfbb569e90242eefed0c6c254f'
